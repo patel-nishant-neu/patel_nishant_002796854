5070a8e2e1ad2f99c2ea864ec96b53d67297ac34
patel_nishant <patel.nishant@northeastern.edu>
2023-02-01 23:52:08 -0500

    02_01

---
+++ GoodFoodBlog _AED_A1/src/model/chef.java
@@ -0,0 +1,63 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author nishn
+ */
+public class chef {
+    
+    private String chefFName;
+    private String chefLName;
+    private String username;
+    private String emailID;
+    private int phoneNum;
+    
+    public chef(){    
+    }
+
+    public String getChefFName() {
+        return chefFName;
+    }
+
+    public void setChefFName(String chefFName) {
+        this.chefFName = chefFName;
+    }
+
+    public String getChefLName() {
+        return chefLName;
+    }
+
+    public void setChefLName(String chefLName) {
+        this.chefLName = chefLName;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmailID() {
+        return emailID;
+    }
+
+    public void setEmailID(String emailID) {
+        this.emailID = emailID;
+    }
+
+    public int getPhoneNum() {
+        return phoneNum;
+    }
+
+    public void setPhoneNum(int phoneNum) {
+        this.phoneNum = phoneNum;
+    }
+    
+
+}
