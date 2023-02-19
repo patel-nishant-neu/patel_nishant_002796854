fc75ba521ac18cf5eba0dab2da5722cdd8babf4c
patel_nishant <patel.nishant@northeastern.edu>
2023-02-18 23:52:41 -0500

    Final Project

---
+++ Patel_Nishant_Assignment2/src/Model/ApplicantCatalog.java
@@ -0,0 +1,88 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+/**
+ *
+ * @author shalomdaniel
+ */
+public class ApplicantCatalog {
+
+    private ArrayList<Applicant> applicants ; 
+
+    public ApplicantCatalog(){
+        this.applicants = new ArrayList<Applicant>();
+    }
+    
+    public ArrayList<Applicant> getApplicants() {
+        return applicants;
+    }
+
+    public void setApplicants(ArrayList<Applicant> applicants) {
+        this.applicants = applicants;
+    }
+    
+    public Applicant createApplicant(String firstname, String lastname, Integer appID, String petName, String petType, String Breed, String petGender, Integer petAge, String vaccine, String vaccineStatus, String app_date ){
+        Applicant applicant = new Applicant();
+        applicant.setOwner_firstname(firstname);
+        applicant.setOwner_lastname(lastname);
+        applicant.setApp_id(appID); 
+        applicant.setApplication_date(app_date); 
+        
+        applicant.getPet().setPet_name(petName);
+        applicant.getPet().setPet_type(petType);
+        applicant.getPet().setBreed(Breed);
+        applicant.getPet().setPet_age(petAge);
+        applicant.getPet().setPet_gender(petGender);
+        applicant.getPetVaccine().setVaccine_name(vaccine);
+        applicant.getPetVaccine().setIsCourseCompleted(vaccineStatus);
+        
+        this.applicants.add(applicant);
+        
+        return applicant;
+        
+    }
+    
+    public boolean checkApplicationIDUnique(Integer ID){
+        for(Applicant m : this.applicants){
+            if(m.getApp_id() == ID){
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    public void removeApplicant(Integer ID){
+          for(Applicant app: this.applicants){
+              if(app.getApp_id() == ID){
+                  this.applicants.remove(app);
+                  break;
+              }
+          }
+    }
+    
+    public Applicant searchApplicantByID(Integer ID){
+          for(Applicant app: this.applicants){
+              if(app.getApp_id() == ID) {
+                  return app;
+              }
+          }
+          return null;
+    }
+    
+    public Applicant searchApplicantByName(String petname){
+          for(Applicant app: this.applicants){
+              if(app.getPet().getPet_name().equals(petname)){
+                  return app;
+              }
+          }
+          return null;
+    } 
+ 
+}
---
+++ Patel_Nishant_Assignment2/src/Model/Insurance.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+/**
+ *
+ * @author shalomdaniel
+ */
+public class Insurance {
+
+    String plan_name;
+    Integer plan_id;
+    Double plan_cost_per_month;
+    Double plan_cost_per_annum;
+    
+    public Insurance(){
+        this.plan_name = "" ;
+//        this.plan_id = null;
+//        this.plan_cost_per_month = null;
+//        this.plan_cost_per_annum = null;
+    }
+    
+    public String getPlan_name() {
+        return plan_name;
+    }
+
+    public void setPlan_name(String plan_name) {
+        this.plan_name = plan_name;
+    }
+
+    public Integer getPlan_id() {
+        return plan_id;
+    }
+
+    public void setPlan_id(Integer plan_id) {
+        this.plan_id = plan_id;
+    }
+
+    public Double getPlan_cost_per_month() {
+        return plan_cost_per_month;
+    }
+
+    public void setPlan_cost_per_month(Double plan_cost_per_month) {
+        this.plan_cost_per_month = plan_cost_per_month;
+    }
+
+    public Double getPlan_cost_per_annum() {
+        return plan_cost_per_annum;
+    }
+
+    public void setPlan_cost_per_annum(Double plan_cost_per_annum) {
+        this.plan_cost_per_annum = plan_cost_per_annum;
+    }
+    
+    public Double calulatePlanCostPerAnnum(Double plan_cost_per_month){
+        plan_cost_per_annum = plan_cost_per_month*12;
+        return plan_cost_per_annum;
+    }
+    
+    
+    @Override
+    public String toString() {
+        return String.valueOf(this.plan_name);
+    }
+    
+}
